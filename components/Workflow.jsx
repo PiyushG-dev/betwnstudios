1156d79ea7b51519188cf925e3d9176f97ec706d
Piyush Goswami <pjgpiyush@gmail.com>
2024-06-16 01:54:39 +0530

    workflow section and animatedList comp  added

---
+++ components/Workflow.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import {
+  CloudArrowUpIcon,
+  LockClosedIcon,
+  ServerIcon,
+} from "@heroicons/react/20/solid";
+import { FlipWords } from "./animation/flip-words";
+import { AnimatedListDemo } from "./AnimatedListDemo";
+const features = [
+  {
+    name: "Push to deploy.",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+    icon: CloudArrowUpIcon,
+  },
+  {
+    name: "SSL certificates.",
+    description:
+      "Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.",
+    icon: LockClosedIcon,
+  },
+];
+const words = ["fast", "smooth", "easy"];
+
+const Workflow = () => {
+  return (
+    <div className="overflow-hidden bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+          <AnimatedListDemo />
+          <div className="lg:pr-8 lg:pt-4">
+            <div className="lg:max-w-lg">
+              <div className="mt-2 text-3xl font-bold tracking-tight sm:text-4xl">
+                Ship
+                <span>
+                  <FlipWords words={words} />
+                </span>
+              </div>
+              <p className="mt-6 text-lg leading-8 text-muted-foreground">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Maiores impedit perferendis suscipit eaque, iste dolor
+                cupiditate blanditiis ratione.
+              </p>
+              <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-muted-foreground lg:max-w-none">
+                {features.map((feature) => (
+                  <div key={feature.name} className="relative pl-9">
+                    <dt className="inline font-semibold text-foreground">
+                      <feature.icon
+                        className="absolute left-1 top-1 h-5 w-5 text-foreground"
+                        aria-hidden="true"
+                      />
+                      {feature.name}
+                    </dt>{" "}
+                    <dd className="inline">{feature.description}</dd>
+                  </div>
+                ))}
+              </dl>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Workflow;

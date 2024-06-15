069c995de786acd8af308234797c2ada7ab666d3
Piyush Goswami <pjgpiyush@gmail.com>
2024-06-16 01:18:55 +0530

    bug fixes

--- components/animation/hero-parallax.jsx
+++ components/animation/hero-parallax.jsx
@@ -95,8 +95,8 @@ export const HeroParallax = ({ products }) => {
 
 export const Header = () => {
   return (
-    <div className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full  left-0 top-0">
-      <h1 className="text-2xl md:text-7xl font-bold dark:text-white">
+    <div className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full left-0 top-0">
+      <h1 className="text-5xl md:text-7xl font-bold dark:text-white">
         Our vision <br /> Your Brand
       </h1>
       <p className="max-w-2xl text-base md:text-xl mt-8 dark:text-neutral-200">
